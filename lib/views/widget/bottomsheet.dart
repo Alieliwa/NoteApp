5055694df63f216f1193c9d10f7b07e6c107dbd6
AliEliwa <alieliwa515>
2022-12-14 09:26:34 +0200

    button ADD

--- lib/views/widget/bottomsheet.dart
+++ lib/views/widget/bottomsheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:notesapp/constans.dart';
 import 'package:notesapp/views/widget/customtextfeild.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -10,18 +11,65 @@ class AddNoteBottomSheet extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18.0),
       child: Column(
-        children:  [
-          SizedBox(height: 24.0.h,),
+        children: [
+          SizedBox(
+            height: 24.0.h,
+          ),
           const CustomTextField(
             hint: 'Title',
           ),
-          SizedBox(height: 24.0.h,),
+          SizedBox(
+            height: 24.0.h,
+          ),
           const CustomTextField(
             hint: 'Content',
-            maxLine: 5,
-          )
+            maxLine: 8,
+          ),
+          SizedBox(
+            height: 24.0.h,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0, left: 10.0,bottom: 10.0),
+            child: defultMaterialButton(
+              width: double.infinity,
+              height: 40.h,
+              text: 'ADD',
+              function: () {},
+            ),
+          ),
         ],
       ),
     );
   }
+
+  Widget defultMaterialButton({
+    double? width,
+    double? height,
+    Color? backColor,
+    double borderRadius = 25,
+    double fontsize = 20,
+    Color ColorText = Colors.black,
+    String? text,
+    VoidCallback? function,
+  }) =>
+      Container(
+        width: width,
+        height: height,
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(borderRadius)),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: MaterialButton(
+          color: kPrimaryColor,
+          hoverColor: Colors.black,
+          onPressed: function,
+          child: Text(
+            text!,
+            style: TextStyle(
+              fontSize: fontsize.sp,
+              color: ColorText,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
 }
